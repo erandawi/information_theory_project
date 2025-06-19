78772d15aa729d563f9e85a17a938c43dcb1b7c2
erandawi <erandalakshan3567@gmail.com>
2025-06-19 19:56:58 +0530

    exception.py ready

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-from src.logging.logger  import logging
-logging.info("hi there Log message from main.py")
+# from src.logging.logger  import logging
+# logging.info("hi there Log message from main.py")
+
+# main.py
+# import sys
+# from src.exception.exception import InformaitonThoeryProjectException
+
+# def test_exception():
+#     try:
+#         a = 1 / 0  # this will raise ZeroDivisionError
+#     except Exception as e:
+#         raise InformaitonThoeryProjectException(e, sys)
+
+# if __name__ == "__main__":
+#     test_exception()
